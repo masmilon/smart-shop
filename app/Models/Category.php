3fcd5655a48131a267fce9280c9e6f0fe6bce7a5
masmilon <masmilon.ginilab@gmail.com>
2022-08-30 08:52:02 +0600

    Completed category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public function parent_category() // CategoryItem -> parent category id
+    {
+        return $this->belongsTo(Category::class, 'parent_category_id');
+    }
+
+    public function child_categories()
+    {
+        return $this->hasMany(Category::class, 'parent_category_id');
+    }
 }
